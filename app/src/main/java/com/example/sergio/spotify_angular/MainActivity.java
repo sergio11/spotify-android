21db95ebc00055974e7fde54c3b42e3dab3b3f8f
Sergio Sánchez <sss4esob@gmail.com>
2016-05-04 17:53:17 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/sergio/spotify_angular/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.sergio.spotify_angular;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
